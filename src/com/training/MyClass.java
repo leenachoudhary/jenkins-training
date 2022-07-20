8b24854b04a50cff3dbfcd8737db15c6c9f0a6a0
lchoudhary <leena@gmail.com>
2022-07-20 16:18:25 +0530

    updated

--- src/com/training/MyClass.java
+++ src/com/training/MyClass.java
@@ -3,7 +3,7 @@ package com.training;
 public class MyClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hi...this is my trail project");
+		System.out.println("Hello...this is my trail project");
 
 	}
 
